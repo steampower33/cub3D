4163d14fb693b5eb8622fec948ecec223f99380f
SeungMinLee <seunlee2@student.42seoul.kr>
2023-12-16 19:31:26 +0900

    pre finish

--- src/main.c
+++ src/main.c
@@ -6,45 +6,12 @@
 /*   By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 16:11:38 by wooseoki          #+#    #+#             */
-/*   Updated: 2023/12/16 19:13:10 by seunlee2         ###   ########.fr       */
+/*   Updated: 2023/12/16 19:30:51 by seunlee2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 #include "parse.h"
-
-void	test(t_map *map)
-{
-	printf("width: %d, height: %d, ceil: %d, floor: %d, door: %d\n", map->width, map->height, map->ceiling, map->floor, map->door_status);
-	int i = 0;
-	while (i < 6)
-	{
-		printf("%s\n", map->info[i]);
-		i++;
-	}
-	printf("prow: %f, pcol: %f, %c\n", map->player_row, map->player_col, map->dir_ch);
-}
-
-void	test_convert_map(t_map *map)
-{
-	int row = 0;
-	int col;
-
-	printf("======================test map===========================\n");
-	while (row < map->height)
-	{
-		col = 0;
-		while (col < map->width)
-		{
-			printf("%d", map->matrix[row][col]);
-			++col;
-		}
-		printf("\n");
-		++row;
-	}
-	test(map);
-	printf("======================test map===========================\n");
-}
 
 void	convert_map_game(t_map *map, t_game *game)
 {
@@ -69,7 +36,6 @@ int	main(int argc, char **argv)
 		error_exit("usage: ./cub3d [map file]\n", 1);
 	init_map(&map, argv[1]);
 	convert_map_game(&map, &game);
-	test_convert_map(&map);
 	init_game(&game);
 	mlx_hook(game.win, X_EVENT_KEY_EXIT, 0, &key_exit, &game);
 	mlx_hook(game.win, X_EVENT_KEY_PRESS, 0, &key_press, &game);
