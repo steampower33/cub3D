22778d84d5b5f6a484b755b683ac9643d8c0b751
SeungMinLee <seunlee2@student.42seoul.kr>
2023-12-16 20:04:54 +0900

    error

--- src/game/error.c
+++ src/game/error.c
@@ -6,7 +6,7 @@
 /*   By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 19:34:58 by seunlee2          #+#    #+#             */
-/*   Updated: 2023/12/16 19:35:47 by seunlee2         ###   ########.fr       */
+/*   Updated: 2023/12/16 20:04:44 by seunlee2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	error_handler(char *message, int status)
 {
+	write(2, "Error\n", 6);
 	write(2, message, ft_strlen(message));
 	write(2, "\n", 1);
 	exit(status);
